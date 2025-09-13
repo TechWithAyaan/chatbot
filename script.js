d5c6f1fcea8b253514c41a82325d7402ac90ab6b
TechWithAyaan <techwithayaanfaheem@gmail.com>
2025-09-13 12:27:49 -0700

    ayaan

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var userValue = document.getElementsByClassName("user")[0];
-var answer =["Ayaan AI","☀️ Sunny Day","I'm fine, thanks for asking!","I was created by Ayaan Web Developer.","Sorry, I don't know the answer."]
+var answer =["Ayaan AI","☀️ Sunny Day","I'm fine, thanks for asking!","I was created by Ayaan Web Developer.","Sorry, I don't know the answer.","‘AyaanWebDeveloper’ is a passionate frontend developer from Pakistan, active on GitHub (169+ repos), sharing projects and tutorials across GitHub, Dev.to, LinkedIn, and Instagram where he focuses on conversion-first design."]
 function send(){
     if(userValue.value.toLowerCase() === "What is your name".toLowerCase()){
        document.getElementsByClassName("userVal")[0].innerHTML = "Q:- "+userValue.value+"?"
@@ -16,6 +16,10 @@ function send(){
    else if(userValue.value.toLocaleLowerCase() === "who created you".toLocaleLowerCase()){
        document.getElementsByClassName("userVal")[0].innerHTML = "Q:- "+userValue.value+"?"
        document.getElementsByClassName("aiAnswer")[0].innerHTML = answer[3]+"."
+    }
+   else if(userValue.value.toLocaleLowerCase() === "who is ayaan".toLocaleLowerCase()){
+       document.getElementsByClassName("userVal")[0].innerHTML = "Q:- "+userValue.value+"?"
+       document.getElementsByClassName("aiAnswer")[0].innerHTML = answer[5]+"."
     }
    else{
        document.getElementsByClassName("userVal")[0].innerHTML = "Q:- "+userValue.value+"?"
